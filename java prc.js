805afc1727fb562b0d7025ca2baecc108670dc72
Bilalkhurram7 <155502659+Bilalkhurram7@users.noreply.github.com>
2024-03-07 18:31:36 +0500

    Add files via upload

---
+++ java prc.js
@@ -0,0 +1,19 @@
+alert("Error! Please enter a valid password.");
+alert("Welcome to JS world...\n Happy Coding!");
+window.alert("Welcome to JS Land");
+window.alert("Happy Coding!\n Prevent this page from additional dalogues")
+console.log("Hello...I can run JS through my browser")
+
+var username="Username?";
+alert(username)
+var myname="Bilal Khurram";
+document.write(myname)
+var message="Hello world";
+alert(message)
+var age="15 years old";
+var occupation="Certified Mobile App Development"
+alert(myname)
+alert(age)
+alert(occupation)
+var pizza="PIZZA"
+alert(pizza)
